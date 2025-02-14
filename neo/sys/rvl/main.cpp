e05a9e8d9b1a3b9927ad451068e09c84f781d742
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2025-02-14 12:57:36 -0100

    Hardcode Wii RAM (All Wii's have 88mb)

--- neo/sys/rvl/main.cpp
+++ neo/sys/rvl/main.cpp
@@ -84,10 +84,7 @@ returns in megabytes
 ================
 */
 int Sys_GetSystemRam( void ) {
-	u32 mem = SYSMEM1_SIZE + SYSMEM2_SIZE;
-	int mb = mem / (1024 * 1024)
-	
-	return mb;
+	return 88;
 }
 
 /*
