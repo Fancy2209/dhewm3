12c7f29350b42d0337e904dec08c7d12ac076cc5
Fancy2209 <64917206+Fancy2209@users.noreply.github.com>
2025-02-14 13:04:29 -0100

    Fix bind

--- neo/sys/rvl/rvl_net.cpp
+++ neo/sys/rvl/rvl_net.cpp
@@ -340,7 +340,7 @@ static int IPSocket( const char *net_interface, int port, netadr_t *bound_to = N
 
 	address.sin_family = AF_INET;
 
-	if ( bind( newsocket, (const struct sockaddr *)&address, sizeof( address ) ) == -1 ) {
+	if ( bind( newsocket, (struct sockaddr *)&address, sizeof( address ) ) == -1 ) {
 		common->Printf( "ERROR: IPSocket: bind: %s\n", strerror( errno ) );
 		close( newsocket );
 		return 0;
